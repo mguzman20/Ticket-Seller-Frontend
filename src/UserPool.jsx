060aac25b57df87cf7be9747e398189fff824b26
JManuelJouanne <89608349+JManuelJouanne@users.noreply.github.com>
2023-08-15 23:33:25 -0400

    Update UserPool.jsx

--- src/UserPool.jsx
+++ src/UserPool.jsx
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js'
 
 const poolData = {
-    UserPoolId: 'us-east-2_zmqMPBwNF',
-    ClientId: '2rlnkgr508lnt22rfmms2q9l8'
+    UserPoolId: USER_POOL,
+    ClientId: CLIENT_ID
 }
 const UserPool = new CognitoUserPool(poolData)
 export default UserPool
